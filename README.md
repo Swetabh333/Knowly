426e504d8d6f21cef4e9daa93bba28b269b54750
Swetabh-Zoomcar333 <swetabh.shreyam@zoomcar.com>
2025-07-25 17:34:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Learning Spring Boot
+
+Repo for spring boot
+
+To use
+
+```shell
+git clone git@github.com:Swetabh333/Knowly.git
+cd Knowly
+mvn spring-boot:run
+```
---
+++ src/main/java/com/learn/knowly/KnowlyApplication.java
@@ -0,0 +1,13 @@
+package com.learn.knowly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowlyApplication.class, args);
+    }
+
+}
